f3a2be544cddfdb6b07651b05c3cfbbbfac8adb2
Gleb Ushakov <101178703+ushakovgleb@users.noreply.github.com>
2025-12-15 23:02:07 -0600

    chore(dev): добавлен plop для генерации компонентов

---
+++ plopfile.js
@@ -0,0 +1,24 @@
+module.exports = function (plop) {
+  plop.setGenerator('component', {
+    description: 'Create React component with jsx + css',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Component name:'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{name}}/{{name}}.jsx',
+        templateFile: 'plop-templates/component.jsx.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{name}}/{{name}}.css',
+        templateFile: 'plop-templates/component.css.hbs'
+      }
+    ]
+  });
+};
